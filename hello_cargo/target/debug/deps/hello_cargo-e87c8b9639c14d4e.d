829a182f0a582e8435107fd4966d5034f75ac654
Lawreros <lawreros@gmail.com>
2021-05-24 18:55:51 -0400

    section 1

---
+++ hello_cargo/target/debug/deps/hello_cargo-e87c8b9639c14d4e.d
@@ -0,0 +1,5 @@
+/home/ross/Documents/Rust/hello_cargo/target/debug/deps/hello_cargo-e87c8b9639c14d4e: src/main.rs
+
+/home/ross/Documents/Rust/hello_cargo/target/debug/deps/hello_cargo-e87c8b9639c14d4e.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/ross/Documents/Rust/hello_cargo/target/debug/hello_cargo: /home/ross/Documents/Rust/hello_cargo/src/main.rs
